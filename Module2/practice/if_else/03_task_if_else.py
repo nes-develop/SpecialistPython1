06c7d602185ecf4d537f52f33238be088e8167f9
nes-develop <42266768+nes-develop@users.noreply.github.com>
2021-07-06 20:51:05 +0300

    Update 03_task_if_else.py

--- Module2/practice/if_else/03_task_if_else.py
+++ Module2/practice/if_else/03_task_if_else.py
@@ -3,3 +3,16 @@
 # Формат выходных данных: Выведите «YES», если треугольник равнобедренный, и «NO» в противном случае.
 
 # TODO: your code here
+
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+
+if number1 == number2:
+    print("Yes")
+elif number2 == number3:
+    print("Yes")
+elif number1 == number3:
+    print("Yes")
+else:
+    print("No")
