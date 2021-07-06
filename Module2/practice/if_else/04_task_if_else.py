c61d24c959c4511fc1f3a2e1b64c4a022bec79d7
nes-develop <42266768+nes-develop@users.noreply.github.com>
2021-07-06 21:01:00 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -5,3 +5,14 @@
 # Для всех остальных случаев не выводить ничего.
 
 # TODO: your code here
+
+number = int(input())
+
+
+
+if number % 3 == 0 and number % 5 == 0:
+    print("Foobar")
+elif number % 3 == 0:
+    print("Foo")
+elif number % 5 == 0:
+    print("Bar")
